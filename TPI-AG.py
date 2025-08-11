82cdec4c6e7efa9c0dafb2d1238876655fdf5c45
Kimey Jeandrevin <jeandrevink@gmail.com>
2025-08-11 16:26:53 -0300

    Chb01 y terminado pseudocodigo 1

--- TPI-AG.py
+++ TPI-AG.py
@@ -2,11 +2,12 @@ import pyedflib
 import random
 import numpy as np
 import random
+import bisect
 
 
-def seizure_detection(Flo,Fhi,Wbg,Wfg,Te,d,Lmin,Tc):
+def seizure_detection(Flo,Fhi,Wbg,Wfg,Te,d,Lmin,Tc,file): # Detect seizures with the given parameters
     seizures = []
-    file = pyedflib.EdfReader("chb01_03.edf")
+    file = pyedflib.EdfReader(f"{file}.edf") # Read the edf the file
     n_channels = file.signals_in_file
     for ch in range(n_channels):
         signals = file.readSignal(ch)
@@ -21,30 +22,68 @@ def seizure_detection(Flo,Fhi,Wbg,Wfg,Te,d,Lmin,Tc):
     return seizures
 
 
-def aggregation(seizures,Tc):
-    print("xd")
+def aggregation(seizures,Tc,n_channels): # Detect seizures across the differents channels
+    evs = []
+    channels = set()
+    detected = []
+    for i in range(n_channels):
+        for ran in seizures[i]:
+            l = ran[0]
+            r = ran[1]
+            evs.append([l,i,1,r]) #1 -> Add a range
+            evs.append([r+1,i,0,l]) #0 -> Delete a range
+    evs = sorted(evs)
+    for i in range(len(evs)): # Sweep line to check inteserctions
+        if evs[i][2] == 0:
+            [r,idx,op,l] = evs[i]
+            if [idx,l,r-1] in channels: channels.remove([idx,l,r-1])
+        else:
+            [l,idx,op,r] = evs[i]
+            channels.add([idx,l,r])
+        if len(channels) >= Tc:
+            lefts = []
+            rights = []
+            for [idx,l,r] in channels:
+                lefts.append(l)
+                rights.append(r)
+            lefts = sorted(lefts)
+            rights = sorted(rights)
+            ld = 0
+            rd = 0
+            if (len(lefts)%2 == 0):
+                ld = (lefts[len(lefts)/2]+lefts[(len(lefts)+1)/2])/2 # Median of a set with even length
+            else:
+                ld = lefts[(len(lefts)+1)/2] # Median of a set with odd length
+            if (len(rights)%2 == 0):
+                rd = (rights[len(rights)/2]+rights[(len(rights)+1)/2])/2 # Median of a set with even length
+            else:
+                rd = rights[(len(rights)+1)/2] # Median of a set with odd length
+            detected.append([ld,rd])
+            channels.clear()
+    return sorted(detected)
 
-def group_seizure(seizure_points,d,Lmin):
+
+def group_seizure(seizure_points,d,Lmin): # Group points that belong to the same seizure
     groups = []
     l = -1
     i = 0
     while (i < len(seizure_points)):
         l = seizure_points[i]
-        while(i+1 < len(seizure_points) and seizure_points[i+1]-l <= d):
+        while(i+1 < len(seizure_points) and seizure_points[i+1]-l <= d): # While the distance between the points is <= d
             i = i+1
         r = seizure_points[i]
         i = i+1
-        groups.append([l,r])
+        groups.append([l,r]) # Add new seizure
     filtered = []
     for i in groups:
         l = groups[i][0]
         r = groups[i][1]
-        if (r-l >= Lmin):
+        if (r-l >= Lmin): # Filter out seizures shorter than Lmin
             filtered.append([l,r])
     return filtered
 
 
-def compute_points(energies,Te):
+def compute_points(energies,Te): # Detect abnormal points
     ordered = sorted(energies)
     seizure_points = []
     percentile = np.percentile(ordered,Te)
@@ -54,45 +93,58 @@ def compute_points(energies,Te):
     return seizure_points
 
 
-def compute_energies(signals,backgrounds,foregrounds):
+def compute_energies(signals,backgrounds,foregrounds): # Calculate energy ratio for every foreground and background window
     energies = []
+    bgs = []
     idx = 0
-    for bg in backgrounds:
-        Lbg = bg[0]
-        Rbg = bg[1]
+    for bg in backgrounds: # Calculate energy of background windows
+        Lbg = bg[0] # Left bound of the background window
+        Rbg = bg[1] # Right bound of the background window
         Ebg = 0
         for i in range(Lbg,Rbg):
             Ebg = Ebg + (signals[i]*signals[i])
-        Ebg = Ebg/(Rbg-Lbg)
-        Lfg = foregrounds[idx][0]
-        Rfg = foregrounds[idx][1]
-        while(Lfg >= Lbg and Lfg < Rbg): # Inclusive-Exclusive -> [,)
-            Efg = 0
-            for i in range(Lfg,Rfg):
-                Efg = Efg + (signals[i]*signals[i])
-            Efg = Efg/(Rfg-Lfg)
-            energies.append(Efg/Ebg)
-            idx = idx + 1
-            if (idx > len(foregrounds)):
-                break
+        Ebg = Ebg/(Rbg-Lbg) # Energy of the background window
+        bgs.append(Ebg)
+    for fg in foregrounds:
+        Lfg = fg[0] # Left bound of the foreground window
+        Rfg = fg[1] # Right bound of the foreground window
+        Lbg = backgrounds[idx][0] # Left bound of the background window
+        Rbg = backgrounds[idx][1] # Right bound of the background window
+        Efg = 0
+        for i in range(Lfg,Rfg):
+            Efg = Efg + (signals[i]*signals[i])
+        Efg = Efg/(Rfg-Lfg) # Energy of the foreground window
+        if (Lfg > Rbg): # If the foreground does not intersect the background window
+            idx = idx+1
+            Lbg = backgrounds[idx][0] # Update left bound
+            Rbg = backgrounds[idx][1] # Update right bound
+        if (Lfg >= Lbg and Rfg <= Rbg): # If the foreground is fully contained in the background
+            energies.append(Efg/bgs[idx])  # Energy ratio
+        if (Lfg <= Rbg and Rfg > Rbg): # If the foreground is parcially contained in the background
+            conA = (Rbg-Lfg)/(Rfg-Lfg) # Porcentage contained in the first background window
+            idx = idx+1
+            Lbg = backgrounds[idx][0] # Update left bound
+            Rbg = backgrounds[idx][1] # Update right bound
+            conB = (Rfg-Lbg)/(Rfg-Lfg) # Porcentage contained in the second background
+            energies.append(Efg/((bgs[idx-1]*conA)+(bgs[idx]*conB))) # Energy ratio
     return energies
 
         
-def segment(signals,longi):
+def segment(signals,longi): # Divide the signals in segments
     segments = []
     tam = len(signals)
     l = 0
     while(l < tam):
-        if (l+longi < tam):
+        if (l+longi < tam): # If is possible to construct a segment of lenght tam
             segments.append([l,l+longi])
             l = l+longi
-        else:
-            segments.append([l,tam-l])
+        else: # Is is not possible to construct a segment of lenght tam
+            segments.append([l,tam-l]) #
             l = tam
     return segment
 
 
-def filterOut(signals,Flo,Fhi):
+def filterOut(signals,Flo,Fhi): # Filter out signal below Flo and above Fhi
     filtered = []
     for i in signals:
         if (i>Flo and i<Fhi):
@@ -105,7 +157,17 @@ def seizures_overlap(seizure1, seizure2):
     start2, end2 = seizure2 
     return max(start1, start2) < min(end1, end2)
 
-def compute_fitness(idx,real_seizures, detected_seizures): 
+def compute_fitness(chromosome):
+    [Flo,Fhi,Wfg,Wbg,Te,d,Lmin,Td] = transform_to_params(chromosome) # Get parameters from the chromosome
+    detected_seizures = seizure_detection(Flo,Fhi,Wbg,Wfg,Te,d,Lmin,Td)
+    samples = ["chb01","chb05","chb12","chb13"]
+    tams = [42,39,42,62]
+    real_seizures = []
+    real_seizures.append([[[0,0]],[[0,0]],[[2996,3036]], [[1467,1494]]], [[0,0]],[[0,0]],[[0,0]],[[0,0]],[[0,0]],[[0,0]],
+                         [[0,0]],[[0,0]],[[0,0]],[[0,0]],[[1732,1772]],[[1015,1066]],[[0,0]],[[1720,1810]],[[0,0]],[[0,0]],
+                         [[327,420]],[[0,0]],[[0,0]],[[0,0]],[[0,0]],[[1862,1963]],[[0,0]],[[0,0]],[[0,0]],[[0,0]],
+                         [[0,0]],[[0,0]],[[0,0]],[[0,0]],[[0,0]],[[0,0]],[[0,0]],[[0,0]],[[0,0]],[[0,0]],[[0,0]],[[0,0]]) #Chb01
+    # Falta hacer lo mismo para los otros pacientes chequeando su summary.txt
     matched_real = set()
     matched_detected = set()
     e = 0 #error de los limites 
@@ -125,6 +187,40 @@ def compute_fitness(idx,real_seizures, detected_seizures):
     return fitness 
 
 
+def transform_to_params(chromosome):
+    Flo = 0, Fhi = 256, Wfg=0 ,Wbg=0,Te=0,d=0,Lmin=0,Td=0
+    x = 0
+    for i in range(0,6): # 6 bits for Flo
+        Flo = Flo + (chromosome[i]<<i)
+    for i in range(0,6): # 6 bits for Fhi
+        Fhi = Fhi - (chromosome[6+i]<<i)
+    for i in range(0,4): # 4 bits for Wfg
+        x = x + (chromosome[12+i]<<i)
+    Wfg = 0.5 + (x/2)
+    x = 0
+    for i in range(0,5): # 5 bits for Wbg
+        x = x + (chromosome[16+i]<<i)
+    Wbg = 60*(1+x)
+    x = 0
+    for i in range(0,6): # 6 bits for Te
+        x = x + (chromosome[21+i]<<i)
+    Te = 100 - (x/10)
+    x = 0
+    for i in range(0,5): # 5 bits for d
+        x = x + (chromosome[27+i]<<i)
+    d = 1 + x
+    x = 0
+    for i in range(0,5): # 5 bits for Lmin
+        x = x + (chromosome[32+i]<<i)
+    Lmin = 1+x
+    x = 0
+    for i in range(0,4): # 4 bits for Td
+        x = x + (chromosome[37+i]<<i)
+    Td = (1+x)/16
+    return [Flo,Fhi,Wfg,Wbg,Te,d,Lmin,Td]
+
+
+
 # Generate a chromosome with the given number of bits
 def generate_chromosome(bits):
     chromosome = [random.randint(0, 1) for _ in range(bits)]
@@ -136,14 +232,20 @@ def generate_random_population(num_chromosomes, bits):
     return [generate_chromosome(bits) for _ in range(num_chromosomes)]
 
 
-def selectedByTournament(population, fit):    
+def selectedByTournament(population, fit,tam_tournament):    
     selected = []
     tam = len(fit)
     for _ in range(tam):
-        idA = random.randint(0,tam-1)
-        idB = random.randint(0,tam-1)
-        if (fit[idA] > fit[idB]): selected.append(population[idA]) 
-        else: selected.append(population[idB]) 
+        to_battle = []
+        best = np.inf
+        idBest = 99999
+        for i in range(tam_tournament):
+            to_battle.append(random.randint(0,tam-1))
+        for i in to_battle:
+            if (fit[i] < best or (fit[i]==best and i<idBest)): 
+                 best = fit[i]
+                 idBest = i
+        selected.append(population[idBest]) 
     return selected 
 
 
@@ -152,7 +254,7 @@ def reproduce(selected,prob_cross):
     for i in range(0, len(selected) - 1,2):
         prob = random.random()
         if (prob <= prob_cross): 
-            cut = random.randint(0,29) 
+            cut = random.randint(0,40) 
             father1 = selected[i]
             father2 = selected[i+1]
             
@@ -173,8 +275,8 @@ def mutation(childs,prob_mut):
         new_child = ""
         prob = random.random()
         if (prob <= prob_mut): 
-            lim_inf = random.randint(0,29) 
-            lim_sup = random.randint(0,29) 
+            lim_inf = random.randint(0,40) 
+            lim_sup = random.randint(0,40) 
 
             if (lim_inf > lim_sup): lim_inf, lim_sup = lim_sup, lim_inf  
             
@@ -185,26 +287,26 @@ def mutation(childs,prob_mut):
         else: new_childs.append(child) 
     return new_childs 
 
+
 def getBestIndividual(popu):
-    mini = 1000000000000
+    mini = np.inf
     idMini = 0
     for i in range(len(popu)):
-        if (compute_fitness(popu[i]))
+        fit = compute_fitness(popu[i]) 
+        if (fit < mini):
+            mini = fit
+            idMini = i
+    return [popu[idMini],mini]
 
 
-def genetic_algorithm(size_population, tam_tournament, mutation_rate): 
-    population = generate_random_population(size_population)
-    real_seizure = []
+def genetic_algorithm(size_population, tam_tournament, mutation_rate,cross_rate): 
+    population = generate_random_population(size_population,41)
     for i in range(10):
         selected = []
-        fitnesses = []
-        detected_per_chromo = []
-        for i in range(len(population)): 
-            [Flo,Fhi,Wbg,Wfg,Te,d,Lmin,Td] = transform_to_params() #Decodificar los parametros del cromosoma
-            detected_per_chromo.append() #Calcular los ataques con los parametros dados
-        for i in range(len(population)): fitnesses.append(compute_fitness(i,real_seizures, detected_seizures))
+        fitnesses = []   
+        for i in range(len(population)): fitnesses.append(compute_fitness(population[i]))
         while len(selected) < size_population:
-            parents = selectedByTournament(population, fitnesses)
+            parents = selectedByTournament(population, fitnesses,tam_tournament)
             childs = reproduce(parents, cross_rate)
             selected = mutation(childs, mutation_rate)
         population = selected
@@ -212,7 +314,7 @@ def genetic_algorithm(size_population, tam_tournament, mutation_rate):
 
 
 if __name__ == "__main__":
-    print("xd")
+    print(genetic_algorithm(30,5,0.01,0.3))
 
 
 
